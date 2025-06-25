2a153179a1ca56ec1f4d42daca4669d8ec35cf81
xiaoBu <1601051004@qq.com>
2025-06-25 11:51:08 +0800

    登录逻辑伪代码

--- frontend/api/src/main/java/com/itheima/prize/api/action/LoginController.java
+++ frontend/api/src/main/java/com/itheima/prize/api/action/LoginController.java
@@ -37,6 +37,19 @@ public class LoginController {
     })
     public ApiResult login(HttpServletRequest request, @RequestParam String account,@RequestParam String password) {
         //TODO：任务3.1-登录模块-用户登录
+        /**
+         * 1、密码错误5次的校验
+         *   1）从redis中获取用户名密码错误次数(redisUtil.get())
+         * 2、用户名密码校验
+         *   1）从数据库中查询用户名密码
+         *      使用MP的语法查询用户(userService.list(wrapper);)
+         * 3、如果用户不为空，保存会话并返回登录信息
+         *   1）保存用户信息到session中(request.getSession().setAttribute())
+         *   2）返回登录信息user
+         * 4、如果用户为空，返回错误信息，并记录错误次数
+         *   1）记录错误次数(redisUtil.incr(),redisUtil.expire())
+         *   2）返回错误信息
+         */
         return null;
     }
 
@@ -44,6 +57,10 @@ public class LoginController {
     @ApiOperation(value = "退出")
     public ApiResult logout(HttpServletRequest request) {
         //TODO：任务3.2-登录模块-用户退出
+        /**
+         * 1、如果session不为空，直接删除session(session.invalidate();)
+         * 2、如果session为空，直接返回成功
+         */
         return null;
     }
 
